52ed5cfd0cabf811202ccbc0add79d619561a70d
Brandon O'Connor <boconnor20@stuy.edu>
2020-11-06 12:23:29 -0500

    added the findOverlap() method

--- Demo.java
+++ Demo.java
@@ -7,14 +7,32 @@ public class Demo{
       }
     }
     }
+  public static SuperArray findOverlap(SuperArray a, SuperArray b){
+    SuperArray returnSA= new SuperArray(a.size()+b.size());
+    for (int i=0; i<a.size();i++){
+      if(b.contains(a.get(i))){
+          returnSA.add(a.get(i));
+      }
+    }
+    removeDuplicates(returnSA);
+    return returnSA;
+  }
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
     words.add("una");    words.add("ebi");     words.add("toro");
+    SuperArray numbers = new SuperArray();
+    numbers.add("9");    numbers.add("2");     numbers.add("2");     numbers.add("3");
+    numbers.add("4");    numbers.add("1");
+    SuperArray numbers2= new SuperArray();
+    numbers2.add("0");    numbers2.add("4");  numbers2.add("2");      numbers2.add("2");
+    numbers2.add("9");
 
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+    System.out.println(findOverlap(numbers,numbers2));
+  }
 }
