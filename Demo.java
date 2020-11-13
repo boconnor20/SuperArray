ec65df07d80a6e6b4ef77caebedca46d4267fc6f
Brandon O'Connor <boconnor20@stuy.edu>
2020-11-12 20:54:15 -0500

    try/catches for set and get

--- Demo.java
+++ Demo.java
@@ -57,12 +57,28 @@ public class Demo{
     removeDuplicates(words);
     System.out.println(words);
     System.out.println(findOverlap(numbers,numbers2));*/
+
     try{SuperArray OogaBooga=new SuperArray(-10);
     System.out.println(OogaBooga);
     }
     catch (IllegalArgumentException e){
       System.out.println("Woah buddy we don't do that here");
     }
+
+    try{SuperArray OogaBooga2=new SuperArray(2);
+    System.out.println(OogaBooga2.get(3));
+    }
+    catch (IndexOutOfBoundsException e){
+      System.out.println("Whoa out of this world");
+    }
+
+    try{SuperArray OogaBooga3=new SuperArray(2);
+    System.out.println(OogaBooga3.set(3, "Booga"));
+    }
+    catch (IndexOutOfBoundsException e){
+      System.out.println("Whoa out of this world");
+    }
+
     SuperArray a= new SuperArray();
     a.add("a"); a.add("b"); a.add("c"); a.add("d"); a.add("e"); a.add("f");
     SuperArray b= new SuperArray();
